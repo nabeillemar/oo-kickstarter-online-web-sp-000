0e27fb2fa6bf5b2d4dba00df9aaee60541269a89
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-07 00:07:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer 
-  attr_accessor :name 
+  attr_accessor :name, :backed_projects
   
   def initialize(name)
-    
-  
+    @name = name 
+    @backed_projects = []
+  end 
   
   
 end 
