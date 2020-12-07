02ade624e4a0541ce076dfc2a5bab255af4b0837
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-07 00:46:53 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer 
+  attr_accessor :name, :backed_projects
+  
+  def initialize(name)
+    @name = name 
+    @backed_projects = []
+  end 
+  
+  def back_project(title)
+    @backed_projects << title 
+    title.backers << self 
+  end 
+  
+  
+  
+end 
